19facabdfc2d86e9a19b2f7dd03cda8215f021ef
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-15 15:45:37 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _24.sumStringDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string = ");
+            string symbolWord = Console.ReadLine ();  //here we summ the symbols numbers  (by Asci code table  ) of a given string characters /
+
+            Console.Write("sum of Asci code of all chars = ");
+            int sum = 0;
+            for (int i = 0; i < symbolWord.Length; i++)
+            {
+                sum += symbolWord[i];
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
